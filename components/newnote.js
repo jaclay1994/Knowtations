a6001737a7569e1e2a2f97ed73c9833b28872393
jaclay1994 <jaclay94@gmail.com>
2021-08-16 22:11:48 -0400

    This is the first commit to React Native application Knowtations.

---
+++ components/newnote.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {TextInput} from 'react-native';
+
+class newNote extends Component {
+  render() {
+    return <TextInput></TextInput>;
+  }
+}
+
+export default newNote;
